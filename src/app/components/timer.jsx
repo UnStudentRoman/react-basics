729cd99d74b03f5cd29930febf6c9f2aaf3f7b56
Marius Cristian <marius.cristian@lseg.com>
2024-11-26 19:34:09 +0200

    Adding timer and css and changing react version.

--- src/app/components/timer.jsx
+++ src/app/components/timer.jsx
@@ -1,8 +1,61 @@
-import React from 'react'
+'use client'
+import {useState, useEffect, useRef} from 'react'
+import styles from './timer.module.css'
+
 
 const Timer = () => {
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const intervalId = useRef(null);
+  const startTime = useRef(0);
+
+  useEffect( () => {
+    if (isRunning) {
+      intervalId.current = setInterval(() => {
+        setElapsedTime(Date.now() - startTime.current);
+        }, 10);
+      }
+
+      return () => {
+        clearInterval(intervalId.current);
+      }
+  }, [isRunning]);
+
+  const startTimer = () => {
+    setIsRunning(true);
+    startTime.current = Date.now() - elapsedTime;
+  };
+
+  const stopTimer = () => {
+    setIsRunning(false);
+  };
+
+  const resetTimer = () => {
+    setElapsedTime(0);
+    setIsRunning(false);
+  };
+
+  const displayTime = () => {
+      let hours = `${Math.floor(elapsedTime / (1000 * 60 * 60))}`.padStart(2, "0");
+      let minutes = `${Math.floor(elapsedTime / (1000 * 60) % 60)}`.padStart(2, "0");
+      let seconds = `${Math.floor(elapsedTime / 1000 % 60)}`.padStart(2, "0");
+
+    
+      return `${hours}:${minutes}:${seconds}`
+  };
+
   return (
-    <div>timer</div>
+    <div className={styles.container}>
+      <div className={styles.title}>TIMER</div>
+      <div>
+        <div className={styles.timer}>{displayTime()}</div>
+        <div className={styles.btn_container}>
+          <button className={styles.btn} onClick={startTimer}>Start</button>
+          <button onClick={stopTimer}>Stop</button>
+          <button onClick={resetTimer}>Reset</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
